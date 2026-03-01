e650b7399dfeb92d19dd8f4ba99262dc29105dff
Tatiana Villa Ema <tatiana@tecnologia-facil.es>
2026-03-01 18:30:36 +0100

    Arreglando urls

--- src/main/java/es/tatvil/formula1/repository/EscuderiaRepository.java
+++ src/main/java/es/tatvil/formula1/repository/EscuderiaRepository.java
@@ -2,9 +2,33 @@ package es.tatvil.formula1.repository;
 
 import es.tatvil.formula1.dto.EscuderiaDTO;
 import es.tatvil.formula1.dto.PilotoDTO;
+import es.tatvil.formula1.entity.Escuderia;
+
 import java.util.List;
 
-public interface EscuderiaRepository {
-    List<EscuderiaDTO> findAll();
-    List<PilotoDTO> findPilotosPorEscuderia(Long escuderiaId);
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EscuderiaRepository extends JpaRepository<Escuderia, Long> {
+    // Aquí puedes poner consultas personalizadas si quieres
+	 @Query("SELECT new es.tatvil.formula1.dto.EscuderiaDTO(e.id, e.nombre) FROM Escuderia e")
+	 List<EscuderiaDTO> findAllDTO();  
+	 @Query("""
+			    SELECT new es.tatvil.formula1.dto.PilotoDTO(
+			        p.numero,
+			        p.codigo,
+			        p.nombre,
+			        p.apellido,
+			        e.nombre,
+			        p.nacionalidad
+			    )
+			    FROM PilotoEscuderia pe
+			    JOIN pe.piloto p
+			    JOIN pe.escuderia e
+			    WHERE e.id = :escuderiaId
+			""")
+			List<PilotoDTO> findPilotosPorEscuderia(@Param("escuderiaId") Long escuderiaId);
 }
